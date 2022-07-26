54ac7325b433930127d3574f6e608dde5df4d9d9
Wesley Carvalho <wesleyfcarvalho1@gmail.com>
2022-07-26 19:22:53 -0300

    add category class and add category type

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,13 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid'
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-
-  const category = {
-    name,
-    description,
-    ud: uuidV4()
-  }
+  const category = new Category(name, description, new Date());
   categories.push(category);
 
   return response.status(201).send();
