c9f8e71d2df0bcf0e7173b0757db94c39847c5e2
Wesley Carvalho <wesleyfcarvalho1@gmail.com>
2022-07-27 20:36:53 -0300

    Change CategoriesRepository to be singleton

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import { Category } from "../model/Category";
 import {
   ICategoriesRepository,
@@ -7,8 +8,17 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ description, name }: ICreateCategoryDTO): void {
