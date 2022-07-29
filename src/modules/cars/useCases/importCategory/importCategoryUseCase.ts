bf950c7ded261478d8676085dd7a69e11c2141b7
Wesley Carvalho <wesleyfcarvalho1@gmail.com>
2022-07-29 15:35:45 -0300

    Fix async method to store categories

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -38,7 +38,7 @@ class ImportCategoryUseCase {
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
 
-    categories.map((category) => {
+    categories.map(async (category) => {
       const { name, description } = category;
 
       const existCategory = this.categoriesRepository.findByName(name);
